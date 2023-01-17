1683e48efe9fd7d9fdd45194bde6241fa29b826d
aadikansara <96415393+aadikansara@users.noreply.github.com>
2023-01-17 18:17:19 +0530

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+ 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i],i);
+        }
+        //searching
+        for(int i=0;i<nums.length;i++) {
+        	int num=nums[i];
+        	int rem=target -num;
+        	if(map.containsKey(rem)) {
+        		int index =map.get(rem);
+        		if(index==i)continue;
+        		return new int[] {i,index};
+        		
+        	}
+        }
+        return new int[] {};
+    }
+}
